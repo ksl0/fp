05f49853b8aad87bb3b48b8a1cae17dbc5696a9b
Katie Li <katiesli16@gmail.com>
2014-06-13 19:45:25 -0600

    MyModule

---
+++ SE/GettingStarted/MyModule.scala
@@ -0,0 +1,29 @@
+/* Another comment */
+/** A documentation comment */
+object MyModule {
+  def abs(n: Int): Int = {
+	  if (n < 0) -n
+	  else n
+  }
+
+	
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+	  if (n <= 0) acc
+      else go(n-1, n*acc)
+    go(n, 1)
+  }
+
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+	  val msg = "The %s of %d is %d."
+	  msg.format(name, n, f(n))
+  }
+	
+  def main(args: Array[String]): Unit = {
+	  formatResult("absolute value", -42, abs)
+   	  formatResult("factorial", 7, factorial)
+	}	 
+
+}
